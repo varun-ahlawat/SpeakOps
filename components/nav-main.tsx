90e48f2476718a681628d4356e51141542585468
Shaik Sufyaan <165240985+Shaik-sufyaan@users.noreply.github.com>
2026-02-07 19:59:42 -0500

    Fixed more errors

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -48,9 +48,11 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
+              <SidebarMenuButton asChild tooltip={item.title}>
+                <a href={item.url}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </a>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
